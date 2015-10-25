627a8ac304869ce1920fe3c6f807ea894b414b55
danielschmitz <danieljfa@gmail.com>
2015-10-25 21:41:20 -0200

    Gulp

--- CommentsComponent2/gulpfile.js
+++ CommentsComponent2/gulpfile.js
@@ -1,26 +1,31 @@
- 
+
+var watchify = require('watchify');
 var browserify = require('browserify');
 var babelify = require('babelify');
-
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglifycss = require('gulp-uglifycss');
-
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 
-gulp.task('default', function () {
-  
-  // Arquivos Javascripts
-  browserify({
+var b =  watchify(browserify({
     entries: './src/index.jsx',
     debug: true,
     extensions: ['.jsx'],
     transform: ['babelify']
-  }).bundle()
+}));
+
+b.on('update', process);
+gulp.task('default', process);
+
+function process(){
+
+    gutil.log(gutil.colors.red('Processo Iniciado'));
+
+    b.bundle()
     .pipe(source('main.min.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
@@ -29,11 +34,11 @@ gulp.task('default', function () {
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./public/js/'));
 
-    //Arquivo CSSs
     gulp.src(["./node_modules/bootstrap/dist/css/bootstrap.min.css","./src/style.css"])
-        .pipe(concat('style.css'))
-        .pipe(uglifycss())
-        .pipe(gulp.dest('./public/css/'));
-     
+    .pipe(concat('style.css'))
+    .pipe(uglifycss())
+    .pipe(gulp.dest('./public/css/'));    
 
-});
+    gutil.log(gutil.colors.red('Processo Finalizado'));
+
+}
