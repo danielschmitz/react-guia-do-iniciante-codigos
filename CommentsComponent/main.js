b5b43313e6403e851231a3fb153c6329df76eba5
danielschmitz <danieljfa@gmail.com>
2015-10-25 13:27:24 -0200

    Adicionar Comentário

--- CommentsComponent/main.js
+++ CommentsComponent/main.js
@@ -41,11 +41,40 @@ var CommentList = React.createClass({
   }
 });
 var CommentForm = React.createClass({
+  handleSubmit: function(e){
+      
+      //cancela a propagação do evento
+      e.preventDefault();
+
+      var author = this.refs.author.value.trim();
+      var text = this.refs.text.value.trim();
+
+      if (!text || !author) {
+        return;
+      }
+
+    //TODO: Precisa atualizar o DATA
+
+    this.refs.author.value = '';
+    this.refs.text.value = '';
+    return;
+
+  },
   render: function() {
     return (
-      <div className="commentForm">
-        CommentForm
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Author:</label>
+            <input type="text" className="form-control" 
+                                                            ref="author"/>
+          </div>
+          <div className="form-group">
+           <label htmlFor="text">Text:</label>
+            <textarea className="form-control" 
+                        rows="3" ref="text"></textarea>
+          </div>
+          <input type="submit" value="Post" className="btn" />
+      </form>
     );
   }
 });
