3f8a3bfc0c44f1a0c8878c17c2573ead6db45283
Daniel Schmitz <danieljfa@gmail.com>
2015-10-27 10:14:12 -0200

    gists com ajax

--- gists/main.js
+++ gists/main.js
@@ -1,19 +1,27 @@
 
-
 var UserGist = React.createClass({
+
+  getInitialState:function(){
+      return {
+        data: []
+      };
+  },
+  componentDidMount:function(){
+    $.ajax(this.props.source).done(function(result){
+      this.setState({data:result});
+    }.bind(this));
+  },
 
  render: function() {
     return (
       <div>
         Your gist url: {this.props.source}
+        <br/>
+        <div>Your data has {this.state.data.length} entries</div>
       </div>
     );
   }
-
 });
-
-
-
 ReactDOM.render(
   <UserGist source="https://api.github.com/users/danielschmitz/gists" />,
   content
